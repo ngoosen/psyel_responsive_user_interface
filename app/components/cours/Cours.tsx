68b4880be8bd318b0b86050904322e34691fe70a
Noémie Goosen <noemie.goosen@gmail.com>
2025-09-18 15:04:43 +0200

    feat: implement search bar in cours list

--- app/components/cours/Cours.tsx
+++ app/components/cours/Cours.tsx
@@ -8,25 +8,58 @@ import styles from "../../style/components/cours/Cours.module.scss";
 import { DataContext } from "@/app/hooks/contexts/useData";
 import { COURS } from "../../hooks/useCours";
 
+import SearchBar from "../ui/SearchBar";
 import CoursCard from "./CoursCard";
 
 const firaSansSemiBold = localFont({ src: "../../assets/fonts/FiraSans-SemiBold.ttf" });
 
+function sortAlphabethically(values: COURS[]) {
+  return values.sort((cours1, cours2) => cours1.intitule.localeCompare(cours2.intitule))
+}
+
 export default function Cours() {
   const [displayedCours, setDisplayedCours] = useState<COURS[]>([]);
+  const [noResult, setNoResult] = useState<boolean>(false);
 
   const { cours, } = useContext(DataContext);
 
   useEffect(() => {
-    setDisplayedCours(cours.sort((cours1, cours2) => cours1.intitule.localeCompare(cours2.intitule)));
+    setDisplayedCours(sortAlphabethically(cours));
   }, [cours]);
+
+  function changeSearchHandler(value: string) {
+    setNoResult(false);
+
+    if (value === "") {
+      setDisplayedCours(sortAlphabethically(cours));
+      return;
+    }
+
+    const filtered = cours.filter(c => c.intitule.toLowerCase().includes(value.toLowerCase()));
+
+    if (filtered.length === 0) {
+      setNoResult(true);
+    }
+
+    setDisplayedCours(sortAlphabethically(filtered));
+  }
 
   return (
     <article className={styles.main}>
-      <input type="checkbox" name="" id="" className={firaSansSemiBold.className} />
-      <ul>
-        {displayedCours.map((cour, index) => <CoursCard key={index} cours={cour} />)}
-      </ul>
+      <input type="checkbox" name="" id="" className={`${firaSansSemiBold.className} ${styles.title_input}`} />
+      <section className={styles.content}>
+        <SearchBar onChange={changeSearchHandler} />
+
+        {noResult && (
+          <article className={styles.no_result}>
+            <p>Aucun cours correspondant</p>
+          </article>
+        )}
+
+        <ul>
+          {displayedCours.map((cour, index) => <CoursCard key={index} cours={cour} />)}
+        </ul>
+      </section>
     </article>
   );
 }
